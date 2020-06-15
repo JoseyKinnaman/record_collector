6ce4a7fc16e087e36b2ea13ac8adefd50d34ef55
Jozy Kinnaman <jozypants@gmail.com>
2020-06-15 13:44:26 -0700

    add controller for albums and homepage view.

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,55 @@
+class AlbumsController < ApplicationController
+ 
+  def homepage
+    
+    render :index
+  end
+  
+  def index
+    @albums = Album.all 
+    render :index
+  end
+
+  def new
+    @album = Album.new
+    render :new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to albums_path
+    else  
+      render :new
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @album = Album.find(params[:id])
+    render :show
+  end
+
+  def update
+    @album = Album.find(params[:id])
+    if @album.update(album_params)
+      redirect_to albums_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @album = Album.find(params[:id])
+    @album.destroy 
+  end
+
+  private
+    def album_params
+      params.require(:name, :discog_id, :artist, :cover_image, :link).permit(:id)
+    end
+
+end
