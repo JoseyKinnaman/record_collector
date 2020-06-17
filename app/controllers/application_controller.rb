e0cccbc3bba375e91b871c16aa97f1c66c3eb101
Jozy Kinnaman <jozypants@gmail.com>
2020-06-17 15:35:26 -0700

    attempt to add username, unsucessful, remove username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
     homepage_path
   end
 
+
+
+  # protected
+  # def configure_permitted_parameters
+  #   added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+  #   devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+  #   devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,19 @@ class User < ApplicationRecord
   has_and_belongs_to_many(:albums)
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # validates :username, presence: true, uniqueness: { case_sensitive: false }
+  # attr_writer :login
+
+  # def login
+  #   @login || self.username || self.email
+  # end
+
+  # def self.find_for_database_authentication(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  #   elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+  #     where(conditions.to_h).first
+  #   end
+  # end
 end
